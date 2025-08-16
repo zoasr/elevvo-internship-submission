44ba593342a03b51cd0c535c748adf08040687d0
zoasr <44146518+ZOASR@users.noreply.github.com>
2025-08-16 21:08:43 +0300

    fix(ui)(job-tracker): fix layout when jobs are present

--- job-application-tracker/src/components/JobList.tsx
+++ job-application-tracker/src/components/JobList.tsx
@@ -8,7 +8,13 @@ export default function JobList() {
 
 	return (
 		<>
-			{jobs.length === 0 && (
+			{jobs.length > 0 ? (
+				<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+					{jobs.map((job) => (
+						<JobCard key={job.id} job={job} />
+					))}
+				</div>
+			) : (
 				<div className="flex flex-col gap-8 justify-center items-center w-full h-full text-4xl text-center">
 					No jobs found
 					<Button>
@@ -16,10 +22,6 @@ export default function JobList() {
 					</Button>
 				</div>
 			)}
-			<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-				{jobs.length > 0 &&
-					jobs.map((job) => <JobCard key={job.id} job={job} />)}
-			</div>
 		</>
 	);
 }
